6f13134f49c831d9a052eeaaa081485190326b2e
oytunerdd <oytunerdogan@gmail.com>
2020-10-01 01:39:46 +0300

    Clearing unnecessary dependencies and duplicate items

--- src/test/java/tests/CompleteScenario.java
+++ src/test/java/tests/CompleteScenario.java
@@ -18,54 +18,60 @@ public class CompleteScenario extends BaseTest implements Constant {
     @Test(priority = 1, description = "Visit Thuisbezorgd Hompe Page")
     public void visitHomePage(){
         this.homepage.goHomePage();
-        waitForPageLoad(10);
 
     }
     @Test(priority = 2, description = "Privacy")
     public void acceptPrivacyPolicy(){
         this.homepage.acceptPrivacyPolicy();
     }
+
+    @Test(priority = 3)
     public void searchForARestaurant(){
         this.homepage.enterSearchKey();
         this.homepage.selectAddressItem();
         this.homepage.selectArea();
     }
 
-
-
-    @Test(priority = 2)
+    @Test(priority = 4)
     public void shouldBeRedirectedToRestaurantList() throws Exception {
-        waitForPageLoad(10);
         this.restaurantList.redirectedToRestaurantPage();
         this.restaurantList.checkRestaurantsListed();
     }
 
+    @Test(priority = 5)
     public void selectRestaurant() throws Exception {
         this.restaurantList.clickOnTestRestaurant();
         this.restaurantDetail.checkSwiperMenuLinks();
     }
 
+    @Test(priority = 6)
     public void selectDishToBasketWithNoSideSelection() throws InterruptedException {
         this.restaurantDetail.addRandomProductToBasket();
     }
 
+    @Test(priority = 7)
     public void selectVariantedDish() throws InterruptedException {
         this.restaurantDetail.clickOnVariantedProduct();
         this.restaurantDetail.selectSideDrinkToBasket();
         this.restaurantDetail.selectSideSaladToBasket();
     }
 
+    @Test(priority = 8)
     public void increaseAmountOfProductToAdd(){
         this.restaurantDetail.increaseProductAmount();
     }
+
+    @Test(priority = 9)
     public void decreaseAmountOfProductToAdd(){
         this.restaurantDetail.decreaseProductAmount();
     }
 
+    @Test(priority = 10)
     public void selectDrink(){
         this.restaurantDetail.selectDrinkOption();
     }
 
+    @Test(priority = 11)
     public void checkBasketAndAddToCartButtonValues(){
         this.restaurantDetail.checkAddToCartButtonAndBasket();
     }
